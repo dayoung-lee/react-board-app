9285194d77e9d8dd4cf1cecaeda04fa89286d81b
dayoung-lee <dayoung5213@gmail.com>
2020-01-26 21:50:11 -0500

    Updates

---
+++ precache-manifest.d7ad91c0e546355e25ad07668088808e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd1e83eb1b62be659715efe1263a0d1",
+    "url": "/react-board-app/index.html"
+  },
+  {
+    "revision": "80dc6e4badd1b5ce5f88",
+    "url": "/react-board-app/static/css/main.5769942a.chunk.css"
+  },
+  {
+    "revision": "9819025201279ff78118",
+    "url": "/react-board-app/static/js/2.48d1f61a.chunk.js"
+  },
+  {
+    "revision": "c97e37d5488e4d07529e18e084899b03",
+    "url": "/react-board-app/static/js/2.48d1f61a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80dc6e4badd1b5ce5f88",
+    "url": "/react-board-app/static/js/main.88dfcd47.chunk.js"
+  },
+  {
+    "revision": "62c0de898970b6851911",
+    "url": "/react-board-app/static/js/runtime-main.92480d3c.js"
+  }
+]);
